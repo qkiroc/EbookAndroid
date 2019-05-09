d847c93a62ac7b51b1819f9d0641b2730d7e8c25
qinhaoyan <460222785@qq.com>
2019-05-09 19:47:42 +0800

    5.9

---
+++ app/src/main/java/com/example/ebook/WantReadActivity.java
@@ -0,0 +1,13 @@
+package com.example.ebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WantReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_want_read);
+    }
+}
